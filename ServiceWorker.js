e52b1f002ad4d03cf05022b2eb4f80305dda708a
wenqian157 <yang@arch.ethz.ch>
2024-06-06 13:31:20 +0200

    torsion final test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-20231106_Innovedum-0.1";
 const contentToCache = [
-    "Build/20240517b.loader.js",
-    "Build/20240517b.framework.js",
-    "Build/20240517b.data",
-    "Build/20240517b.wasm",
+    "Build/20240606.loader.js",
+    "Build/20240606.framework.js",
+    "Build/20240606.data",
+    "Build/20240606.wasm",
     "TemplateData/style.css"
 
 ];
